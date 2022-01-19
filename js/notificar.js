c93ec8d60ae6bcd66aa3a68d1a00ceb82c3b5728
maxi barbo <maxi10lb@gmail.com>
2022-01-18 23:11:32 -0300

    notificar.js

--- js/notificar.js
+++ js/notificar.js
@@ -1,35 +1,32 @@
 
 $(function(){
 
-    let inputsThree =  $(".inputsThree");
-    let inputsFour =  $(".inputsFour");
+   // CREAR bloque persona con btn "añadir Persona"
+
+  $('#agregarPersona').on('click', function() {
+    $('.inputsThree').append(` <div class="idatos">
+                                <input class="form-control" placeholder="Nombre de Pila"></input>
+                                <input class="form-control" placeholder="Apellidos"></input>
+                                <input class="form-control" placeholder="Empresa"></input>
+                                <input class="form-control" placeholder="Direccion"></input>
+                                <input class="form-control" placeholder="Mail"></input>
+                                <input class="form-control" placeholder="Telefono"></input>
+                                <input class="form-control" placeholder="DNI"></input>
+                                <input class="form-control" placeholder="Unidad Organizativa"></input>
+                                <button class="remover btn btn-outline-danger">Borrar Persona</button>
+                                </div>`);
+                         
+  });
+  
+  //ELIMINAR Bloque de datos Btn "Añadir Persona"
+  $(document).on('click', '.remover', function(){
+    $(this).parents('.idatos').remove();
     
-    $("#agregarPersona").click(function () {	 
-        
-        inputsThree.prepend( ` <div class="inputsThree">
-                                    <input class="form-control" placeholder="Nombre de Pila"></input>
-                                    <input class="form-control" placeholder="Unidad Organizativa"></input>
-                                    <input class="form-control" placeholder="Direccion"></input>
-                                    <input class="form-control" placeholder="Mail"></input>
-                                    </div>`);
 
-        inputsFour.prepend( `<div class="inputsFour">    
-                            <input class="form-control" placeholder="Apellidos"></input>
-                            <input class="form-control" placeholder="Empresa"></input>
-                            <input class="form-control" placeholder="Telefono"></input>
-                            <input class="form-control" placeholder="DNI"></input>
-                            <div>`)
-
-        
-    });
-
-    $("#borrarPersona").click(function () {	        
-     inputsThree.remove(); 
-     inputsFour.remove();
-    });
-
-
+  })
 });
+
+
 
 
 
